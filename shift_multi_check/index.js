c2c08e43a33c4b8b15655d61626e244618612317
caesiumy <mn0658@naver.com>
2020-08-08 01:19:49 +0900

    feat: Add multi disable check

--- shift_multi_check/index.js
+++ shift_multi_check/index.js
@@ -6,14 +6,30 @@ const handleClick = (e) => {
   let isBetween = false;
   if (e.shiftKey && e.target.checked) {
     checkboxes.forEach((checkbox) => {
+      console.log("handleClick -> checkbox", checkbox);
       if (checkbox === e.target || checkbox === lastChecked) {
         isBetween = !isBetween;
+        console.log("handleClick -> isBetween", isBetween);
       }
 
       if (isBetween) {
         checkbox.checked = true;
       }
     });
+  }
+  if (e.shiftKey && e.target.checked === false) {
+    checkboxes.forEach((checkbox) => {
+      console.log("handleClick -> checkbox", checkbox);
+      if (checkbox === e.target || checkbox === lastChecked) {
+        isBetween = !isBetween;
+        console.log("handleClick -> isBetween", isBetween);
+      }
+
+      if (isBetween) {
+        checkbox.checked = false;
+      }
+    });
+    lastChecked.checked = false;
   }
   lastChecked = e.target;
   console.log("handleClick -> lastChecked", lastChecked);
