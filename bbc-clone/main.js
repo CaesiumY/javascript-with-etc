02f64d7417ffced16dac3d911a155dd2a05db162
caesiumy <mn0658@naver.com>
2020-09-07 22:50:16 +0900

    feat: Add advanced features

--- bbc-clone/main.js
+++ bbc-clone/main.js
@@ -2,11 +2,32 @@
   const steps = document.querySelectorAll(".step");
   const graphics = document.querySelectorAll(".graphic-item");
   let currentItem = graphics[0];
-  let ioIndex;
+  let ioIndex = 0;
 
   const io = new IntersectionObserver((entries, observer) => {
     ioIndex = entries[0].target.dataset.index * 1;
   }); // 현재 화면에 보이는 것만 감시
+
+  const actions = {
+    birdFlies(index) {
+      const bird = document.querySelector(`[data-index="${index}"] .bird`);
+
+      bird.style.transition = `1.5s 0.5s linear`;
+      bird.style.transform = `translateX(${window.innerWidth}px)`;
+    },
+    birdFlies2(index) {
+      const bird = document.querySelector(`[data-index="${index}"] .bird`);
+
+      bird.style.transition = `1.5s 0.5s linear`;
+      bird.style.transform = `translate(${window.innerWidth}px, 
+        ${-window.innerHeight * 0.7}px)`;
+    },
+    resetBird(index) {
+      const bird = document.querySelector(`[data-index="${index}"] .bird`);
+      bird.style.transition = `0s`;
+      bird.style.transform = `translateX(-100%)`;
+    },
+  };
 
   steps.forEach((step, i) => {
     step.setAttribute("data-index", i);
@@ -14,35 +35,46 @@
   });
   graphics.forEach((graphic, i) => graphic.setAttribute("data-index", i));
 
-  const activate = () => {
+  const activate = (index) => {
+    const action = graphics[index].dataset.action;
     currentItem.classList.add("visible");
+    if (action) {
+      actions[action](index);
+    }
   };
-  const inactivate = () => {
+  const inactivate = (index) => {
+    const action = graphics[index].dataset.action;
     currentItem.classList.remove("visible");
+    if (action) {
+      actions.resetBird(index);
+    }
   };
 
   window.addEventListener("scroll", () => {
     let boundingRect;
-    let temp = 0;
 
     steps.forEach((step, i) => {
       if (i > ioIndex + 1 || i < ioIndex - 1) return;
+
       boundingRect = step.getBoundingClientRect();
-      temp++;
 
       if (
         boundingRect.top > window.innerHeight * 0.2 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
-        inactivate();
+        inactivate(currentItem.dataset.index);
 
         currentItem = graphics[step.dataset.index];
-        activate();
+        activate(currentItem.dataset.index);
       }
     });
-
-    console.log("temp", temp);
   });
 
-  activate();
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      scrollTo(0, 0);
+    }, 100);
+  });
+
+  activate(0);
 })(); // 외부에서의 접근을 차단하는 익명 함수 선언
